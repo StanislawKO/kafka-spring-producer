92f50385b30dd78737e6bbe23cce0f079e661109
Stanislaav Kozh <printks@yandex.ru>
2024-07-02 22:14:27 +0300

    Initial commit

---
+++ src/main/java/com/example/kafkaspringproducer/KafkaSpringProducerApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkaspringproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSpringProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaSpringProducerApplication.class, args);
+    }
+
+}
